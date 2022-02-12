94ba0db83abb7ab993c9707a55c43ff160a450ea
Lumbr 990 <jumiciobi@gmail.com>
2022-02-12 15:20:44 +0000

    Initial Commit

---
+++ Assets/SuperUnityBuild/BuildConstants.cs
@@ -0,0 +1,47 @@
+// This file is auto-generated. Do not modify or move this file.
+
+public static class BuildConstants
+{
+    public enum ReleaseType
+    {
+        None,
+        Game_Prototype,
+    }
+
+    public enum Platform
+    {
+        None,
+        PC,
+        macOS,
+        Linux,
+        Android,
+        iOS,
+        WebGL,
+    }
+
+    public enum Architecture
+    {
+        None,
+        Windows_x86,
+        Windows_x64,
+        macOS,
+        Linux_x64,
+        Android,
+        iOS,
+        WebGL,
+    }
+
+    public enum Distribution
+    {
+        None,
+        Manjaro,
+    }
+
+    public static readonly System.DateTime buildDate = new System.DateTime(637802744455646760);
+    public const string version = "0.0.1";
+    public const ReleaseType releaseType = ReleaseType.Game_Prototype;
+    public const Platform platform = Platform.WebGL;
+    public const Architecture architecture = Architecture.WebGL;
+    public const Distribution distribution = Distribution.None;
+}
+
